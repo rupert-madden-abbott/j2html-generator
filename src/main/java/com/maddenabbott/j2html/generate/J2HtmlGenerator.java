44e469e5c83c12fd63534ed8a582be3f2f3af9ff
Rupert Madden-Abbott <rupert@madden-abbott.com>
2018-08-19 16:15:29 +0100

    Parse as HTML otherwise empty elements e.g. meta, don't parse correctly.

--- src/main/java/com/maddenabbott/j2html/generate/J2HtmlGenerator.java
+++ src/main/java/com/maddenabbott/j2html/generate/J2HtmlGenerator.java
@@ -1,17 +1,19 @@
 package com.maddenabbott.j2html.generate;
 
-import org.jsoup.nodes.Document;
+import java.util.List;
+
+import org.jsoup.nodes.Node;
 import org.jsoup.parser.Parser;
 import org.jsoup.select.NodeTraversor;
 
 public class J2HtmlGenerator {
   public String generate(String html) {
     StringBuilder buffer = new StringBuilder();
-    parse(html).childNodes().forEach(node -> NodeTraversor.filter(new J2HtmlNodeFilter(buffer), node));
+    parse(html).forEach(node -> NodeTraversor.filter(new J2HtmlNodeFilter(buffer), node));
     return buffer.toString();
   }
 
-  private Document parse(String html) {
-    return Parser.xmlParser().parseInput(html, "");
+  private List<Node> parse(String html) {
+    return Parser.parseFragment(html, null, "");
   }
 }
